d737a523b77e2411514d6a2976541e0ef4ac23dd
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-01 13:58:10 +0100

    completed advanced exercise 1

---
+++ Jakob Platzer/script-advanced.js
@@ -0,0 +1,32 @@
+// Exercise 1
+
+/* let a = 7;
+
+let b = 14;
+
+let c = "21";
+
+let d = '36';
+
+let e = 42;
+
+let result1 = a + b + Number(c) + Number(d) + e;
+
+let f = '1';
+
+let g = 15;
+
+let h = 8;
+
+let i = "1";
+
+let result2 = Number(f) * g * h * Number(i);
+
+console.log(result1)
+
+console.log(result2)
+
+let finalresult = result1 / result2;
+
+document.write(finalresult); */
+
