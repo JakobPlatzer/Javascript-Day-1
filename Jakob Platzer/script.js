9dee3affdcf6bdaabe878e8a7807c995c835b92e
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-01 11:51:14 +0100

    prepared for presentation

--- Jakob Platzer/script.js
+++ Jakob Platzer/script.js
@@ -1,3 +1,13 @@
+// for basic exercise 2+3
+
 console.log("Hi, my name is Martin")
 
 document.write("Hi, my Name is Martin")
+
+
+// basic exercise 4
+
+// let players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"]
+
+// console.log("The most valuable player of the match is " + players[2] + " !")
+// document.write("The most valuable player of the match is " + players[2] + " !")
