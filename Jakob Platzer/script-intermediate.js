bdfa110e439ac054952963463623ef1d9ba8a841
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-01 13:22:08 +0100

    completed exercise 2

--- Jakob Platzer/script-intermediate.js
+++ Jakob Platzer/script-intermediate.js
@@ -1,5 +1,6 @@
 // intermediate exercise 1
 
+
 let carbrands = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"];
 
 // carbrands.sort();
@@ -7,5 +8,56 @@ let carbrands = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"
 console.log(carbrands);
 document.write(carbrands);
 
-// intermediate exercise 2
+// intermediate exercise 2.1
+
+/*let fruits = ["apple", "banana", "kiwi"];
+
+document.write(fruits);
+
+console.log(fruits);
+
+document.write('<br>');*/
+
+
+/*fruits.push("orange");
+
+console.log(fruits);
+
+document.write(fruits);
+
+document.write('<br>');*/
+
+
+/*fruits.pop();
+
+console.log(fruits);
+
+document.write(fruits);*/
+
+// intermediate exercise 2.2
+
+
+/*let animals = ["monkey", "horse", "dog"];
+
+document.write(animals);
+
+console.log(animals);
+
+document.write('<br>');*/
+
+
+/*animals.unshift("cat");
+
+console.log(animals);
+
+document.write(animals);
+
+document.write('<br>');*/
+
+
+/*animals.shift();
+
+console.log(animals);
+
+document.write(animals);*/
 
