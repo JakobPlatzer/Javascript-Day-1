15bc77d601e4fc41b5080c29d33c80ca5de20d2d
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-01 12:07:28 +0100

    start of intermediate exercise 2

--- Jakob Platzer/script-intermediate.js
+++ Jakob Platzer/script-intermediate.js
@@ -5,3 +5,7 @@ let carbrands = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"
 // carbrands.sort();
 
 console.log(carbrands);
+document.write(carbrands);
+
+// intermediate exercise 2
+
